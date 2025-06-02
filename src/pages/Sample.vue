be1bdef8a44e3c237fd2ca944843ffd28e3452c7
unknown <helloimdevit@gmail.com>
2025-06-02 11:50:13 +0700

    ...

--- src/pages/Sample.vue
+++ src/pages/Sample.vue
@@ -1,2 +1,23 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { useCartStore } from "../stores/cartStore";
+import { useUserStore } from "../stores/userData";
+import { useReceiverStore } from "../stores/receiverStore";
+import { useOrderStore } from "../stores/orderStore";
+
+const cartStore = useCartStore();
+const userStore = useUserStore();
+const receiverStore = useReceiverStore();
+const orderStore = useOrderStore();
+
+function handleClick() {
+  cartStore.$reset();
+  userStore.$reset();
+  receiverStore.$reset();
+  orderStore.$reset();
+}
+</script>
+<template>
+  <button class="btn btn-warning btn-soft mx-auto" @click="handleClick">
+    Click Me!
+  </button>
+</template>
